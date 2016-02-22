fcea4a16fcebe11dd8641975fb440df5e5b15fa2
harini chakrapani <harini.chakrapani@journalism.cuny.edu>
2016-02-22 17:54:37 -0500

    add

---
+++ week2/javascript.js
@@ -0,0 +1,39 @@
+function scroll1(){ 
+  var ele = document.getElementById("souls"); 
+  window.scrollTo(ele.offsetLeft,ele.offsetTop);
+}
+
+
+
+
+function scroll2(){
+
+	document.getElementById("poe").scrollIntoView(true); 
+
+
+
+} 
+
+function scrollIntoView3(){
+
+document.getElementById("dante"); 
+
+
+}
+
+
+function scrollIntoView4(){
+
+document.getElementById("twenty"); 
+
+}
+
+
+
+function scrollIntoView5(){
+
+document.getElementById("wimpy"); 
+
+}
+
+
